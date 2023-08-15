a5faab31ef9af4661b94a7a5a6704d050d93bb88
natalie <wong.lt.natalie@gmail.com>
2023-08-14 20:35:23 -0400

    me escaping from my problems

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const SELECTED_BUTTONBG = "rgba(33, 37, 41, 0.4)";
 
 let color = "#212529";
 let eraser = false;
-let transparency = false;
 let randomColor = false;
 let gridLines = true;
 
@@ -19,7 +18,6 @@ let slider = document.querySelector("#grid-size-slider");
 let sliderValue = document.querySelector("#slider-value");
 
 const eraserButton = document.querySelector(".toggle-eraser");
-const transparencyButton = document.querySelector(".toggle-transparency");
 const randomColorButton = document.querySelector(".toggle-random-color");
 
 // Functions
@@ -36,7 +34,7 @@ function makeGrid(wAndH) {
 
             // event listener
             cell.addEventListener("mouseover", () => {
-                cell.style.background = color;
+                    cell.style.background = color;
             })
         }
     }
@@ -46,26 +44,10 @@ makeGrid(16);
 // reset booleans function
 function resetBooleans() {
     eraser = false;
-    transparency = false;
     randomColor = false;
 
     eraserButton.style.background = DEFAULT_BUTTONBG;
-    transparencyButton.style.background = DEFAULT_BUTTONBG;
     randomColorButton.style.background = DEFAULT_BUTTONBG;
-}
-
-// hex to rgba
-function hexToRGBA(hex) {
-    var c;
-    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-        c= hex.substring(1).split('');
-        if(c.length== 3){
-            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-        c= '0x'+c.join('');
-        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.1)';
-    }
-    throw new Error('Bad Hex');
 }
 
 // Tools (Event Listeners) 
@@ -103,29 +85,16 @@ buttons.forEach((button) => {
         if (e.target.className == "toggle-eraser") {
             eraser = !eraser;
             if (eraser) {
-                transparency = false;
                 randomColor = false;
 
                 e.target.style.background = SELECTED_BUTTONBG;
                 color = "white";
             }
         }
-        else if (e.target.className == "toggle-transparency") {
-            transparency = !transparency;
-            if (transparency) {
-                eraser = false;
-                randomColor = false;
-
-                e.target.style.background = SELECTED_BUTTONBG;
-
-                color = hexToRGBA(color);
-            }
-        } 
         else if (e.target.className == "toggle-random-color") {
             randomColor = !randomColor;
             if (randomColor) {
                 eraser = false;
-                transparency = false;
 
                 e.target.style.background = SELECTED_BUTTONBG;
             }
@@ -149,8 +118,9 @@ buttons.forEach((button) => {
             eraserButton.style.background = DEFAULT_BUTTONBG;
             color = colorPicker.value;
         }
-        if (!transparency) transparencyButton.style.background = DEFAULT_BUTTONBG;
-        if (!randomColor) randomColorButton.style.background = DEFAULT_BUTTONBG;
+        if (!randomColor) {
+            randomColorButton.style.background = DEFAULT_BUTTONBG;
+        } 
 
     })
 
